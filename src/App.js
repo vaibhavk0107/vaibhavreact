a9e3944ae0f4052057c0cb71f6f50a3923eef202
vaibhav kothari <vaibhav@xeniapp.com>
2021-04-19 16:01:36 +0530

    h3 adding

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
           Edit <code>src/App.js</code>.
         </p>
         <h1>This is Vaibhav</h1>
+        <h3>Slider</h3>
         <a
           className="App-link"
           href="https://reactjs.org"
